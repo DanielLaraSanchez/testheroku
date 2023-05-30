c4e4087d96cb39958de06cc0dfcf355b0e67f1d3
Daniel Lara Sanchez <daniellaraedinburgh@hotmail.com>
2023-05-30 22:57:17 +0100

    adedlj

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ const server = http.createServer((req, res) => {
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
